389bc61b61dcc8b7523745cab46f342b80fe1b40
CodingManoj <CodingManoj@gmail.com>
2023-12-20 20:22:06 -0500

    Terraform Basics

--- 03-ec2-instance/ec.tf
+++ 03-ec2-instance/ec.tf
@@ -1,8 +1,17 @@
-resource "aws_instance" "web" {
-  ami           = "ami-06911ef5687b5d1ad"
+resource "aws_instance" "app" {
+  ami           = "ami-06911ef5687b5d1ad"           # argument
   instance_type = "t3.micro"
 
   tags = {
     Name = "DevOps-With-AWS-LabInstance"
   }
+}
+
+output "instance_dns" {
+    value = aws_instance.app.arn.private_dns                                      # attributes
+}
+
+
+output "instance_arn" {
+    value = aws_instance.app.arn                                   # attributes
 }
