16c2cd35c2c525ce47387db076c32525f553e042
CodingManoj <CodingManoj@gmail.com>
2023-12-20 20:37:17 -0500

    Terraform Basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -1,41 +1,42 @@
 resource "aws_instance" "app" {
-  ami           = "ami-06911ef5687b5d1ad"                                     # argument
-  instance_type = "t3.micro"
+  ami                    = "ami-06911ef5687b5d1ad"                                     # argument
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["aws_security_group.allow_tls.id"]
 
-  tags = {
-    Name = "DevOps-With-AWS-LabInstance"
-  }
+    tags = {
+             Name        = "DevOps-With-AWS-LabInstance"
+    }
 }
 
 output "instance_dns" {
-    value = aws_instance.app.private_dns                                      # attributes
+    value               = aws_instance.app.private_dns                                      # attributes
 }
 
 output "instance_arn" {
-    value = aws_instance.app.arn                                             # attributes
+    value               = aws_instance.app.arn                                             # attributes
 }
 
 
 resource "aws_security_group" "allow_tls" {
-  name        = "b56_allow_tls"
-  description = "B56 Allow TLS inbound traffic"
+  name                  = "b56_allow_tls"
+  description           = "B56 Allow TLS inbound traffic"
 
   ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+       description      = "SSH from VPC"
+       from_port        = 22
+       to_port          = 22
+       protocol         = "tcp"
+       cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+       from_port        = 0
+       to_port          = 0
+       protocol         = "-1"
+       cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "allow_tls"
-  }
+    tags = {
+      Name = "allow_tls"
+    }
 }
