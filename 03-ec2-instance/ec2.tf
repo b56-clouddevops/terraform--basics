4c4a854c2d73ec44ca65597e7a345243807efcd5
CodingManoj <CodingManoj@gmail.com>
2023-12-20 20:38:31 -0500

    Terraform Basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app" {
   ami                    = "ami-06911ef5687b5d1ad"                                     # argument
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["aws_security_group.allow_tls.id"]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
     tags = {
              Name        = "DevOps-With-AWS-LabInstance"
