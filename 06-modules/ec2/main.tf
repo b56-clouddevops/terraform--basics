e0fae40b9a66a203b7c53fd1b488548166e8262b
CodingManoj <CodingManoj@gmail.com>
2023-12-26 06:56:13 +0530

    Terraform Basics

--- 06-modules/ec2/main.tf
+++ 06-modules/ec2/main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "app" {
   ami                    = "ami-06911ef5687b5d1ad"                                     # argument
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [var.sg]
+  vpc_security_group_ids = [var.sg]                                                    # Start using the variable
 
   tags = {
     Name = "EC2-From-Modules"
   }
 }
 
-#  Declare that Input that you need to consume as variable
+# Step 3:  Declare that Input that you need to consume as variable
 variable "sg" {}
 
 
