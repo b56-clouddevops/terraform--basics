9c115500003efb153a357ef1b211503578becb35
CodingManoj <CodingManoj@gmail.com>
2023-12-27 07:13:50 +0530

    Optimizing Jenkins Shared Library Auto Push

--- 07-functions/main.tf
+++ 07-functions/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
   ami                    =  data.aws_ami.ami.id                                # argument
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allows_tls.id]
 
     tags = {
              Name        = "AWSCloud-LabInstance"
