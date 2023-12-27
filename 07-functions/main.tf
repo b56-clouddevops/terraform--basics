4a6d23741d37ba05dfc1d7f522ed1d55d9d58053
CodingManoj <CodingManoj@gmail.com>
2023-12-27 07:16:42 +0530

    Optimizing Jenkins Shared Library Auto Push

--- 07-functions/main.tf
+++ 07-functions/main.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "web" {
+  count                  = 3
+
   ami                    =  data.aws_ami.ami.id                                # argument
   instance_type          = "t3.micro"
 
